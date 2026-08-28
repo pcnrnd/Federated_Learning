c6702157124df94d9af6b739ac96fda1e300b793
이진웅 <jwlee@pcninc.co.kr>
2026-08-28 11:19:32 +0900

    modify vite.config.ts

---
+++ backup/poc2/frontend/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: '../static/dist',
+    emptyOutDir: true,
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8010',
+        changeOrigin: true,
+      },
+    },
+  },
+});
