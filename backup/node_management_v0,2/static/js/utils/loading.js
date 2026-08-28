edf92c5cb12ef0c944f9e1c61d94b331e998e53e
이진웅 <jwlee@pcninc.co.kr>
2026-08-28 10:57:05 +0900

    modify loading.js

--- backup/node_management_v0,2/static/js/utils/loading.js
+++
@@ -1,9 +0,0 @@
-/** 로딩 오버레이 유틸리티 */
-export function showLoading() {
-  document.getElementById('loadingOverlay').style.display = 'flex';
-}
-
-export function hideLoading() {
-  document.getElementById('loadingOverlay').style.display = 'none';
-}
-
